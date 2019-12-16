96484e3118653d6aee7773f58addbad9bf6f7fb0
artureduard0 <arturgpr2@gmail.com>
2019-12-16 10:52:39 -0300

    v2

--- app/src/main/java/com/example/myapplication/opcoes.java
+++ app/src/main/java/com/example/myapplication/opcoes.java
@@ -2,7 +2,9 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class opcoes extends AppCompatActivity {
 
@@ -10,5 +12,11 @@ public class opcoes extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main2);
+    }
+
+    public void abrirTelaInformacoes(View view){
+        Intent intent;
+        intent = new Intent(this, infoConta.class);
+        startActivity(intent);
     }
 }
